979f1f691467de13b282585c2046b33b11175cbe
OussamaRZIGUEN <rzoussama98@gmail.com>
2024-12-13 09:59:02 +0000

    commit-4

--- src/main/java/org/example/git_playlist/GitPlaylistApplication.java
+++ src/main/java/org/example/git_playlist/GitPlaylistApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class GitPlaylistApplication {
 
+	public String purchase(String username, String amount, String productName) {
+		return "HI am"+ username+ " I ....";
+	}
+
 	public void addData(String data) {
 
 		if(data.equals(Constant.data)) {
 			System.out.println("Java/Spring");
 		}
-		
+
 		System.out.println("Others");
 	}
 
