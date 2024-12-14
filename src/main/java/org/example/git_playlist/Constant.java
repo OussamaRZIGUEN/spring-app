0d71533ee4559f90fe73e5d3696ec525671a390d
OussamaRZIGUEN <151402566+OussamaRZIGUEN@users.noreply.github.com>
2024-12-14 23:55:58 +0100

    Update Constant.java

--- src/main/java/org/example/git_playlist/Constant.java
+++ src/main/java/org/example/git_playlist/Constant.java
@@ -7,4 +7,6 @@ public class Constant {
     public static final String data = "Java/Spring";
 
     private static final String name ="Java";
+
+    private static final String weirdMessage ="Message-1";
 }
