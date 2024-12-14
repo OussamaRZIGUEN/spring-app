d4cd092bd5fc245dcd59d2bc2e050e0f6eaf64bd
OussamaRZIGUEN <rzoussama98@gmail.com>
2024-12-14 22:44:55 +0000

    co

--- src/main/java/org/example/git_playlist/Constant.java
+++ src/main/java/org/example/git_playlist/Constant.java
@@ -2,5 +2,9 @@ package org.example.git_playlist;
 
 public class Constant {
 
+    public static final String MY_GITHUB_PASSWORD = "Oussama@2030";
+
     public static final String data = "Java/Spring";
+
+    private static final String name ="Java";
 }
